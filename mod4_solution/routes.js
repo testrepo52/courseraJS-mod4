d67edf5889763ab5080c5f5f61006d16a5831c57
Aishwarya Sharma <ishans632@gmail.com>
2018-04-19 18:32:01 +0530

    asfd

--- mod4_solution/routes.js
+++ mod4_solution/routes.js
@@ -20,14 +20,14 @@
       // State for home 
   		.state('home', {
   			url: '/', 
-  			templateUrl: "../js/home.template.html"
+  			templateUrl: "/src/home.template.html"
   		})
 
 
       // State for categories 
   		.state('categories', {
   			url: '/categories', 
-  			templateUrl: "/js/categories/categories.template.html",
+  			templateUrl: "/src/categories.template.html",
         controller: "CategoryController as categoryList", 
         resolve : {
           items : ['MenuDataService', function(MenuDataService){
@@ -44,7 +44,7 @@
 
       .state('items', {
         url : '/items/{itemId}', 
-        templateUrl: '../js/items/items.template.html', 
+        templateUrl: '/src/items.template.html', 
         controller: "ItemDetailController as itemDetail", 
       resolve: {
           items: ['$stateParams', 'MenuDataService',
